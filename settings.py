cda0d1fb1310195def8353ac027d974dcd3ca7e4
mukhammadayyubkhon <mukhammadayyubkhon_bahodirkhonov@student.itpu.uz>
2026-07-26 14:13:43 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -37,7 +37,7 @@ MAX_BALL_SPEED_X = 8
 
 # --- Bonuses ---------------------------------------------------------------------
 BONUS_PROBABILITY = 0.3  # Chance that destroyed brick will drop a bonus
-BONUS_TYPES = ["extend", "multiball", "laser", "extra_life"]
+BONUS_TYPES = ["extend", "multiball", "laser", "extra_life", "shrink", "speed_up", "speed_down"]
 
 # --- Visual Effects -----------------------------------------------------------
 TRAIL_LENGTH = 6  # Ball's Motion Trail Length
